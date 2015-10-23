f1858e291d95d5bf260beb61e02a665bf33b4cec
Zachary Huff <zach.huff.386@gmail.com>
2015-10-23 00:24:29 -0400

    Update build script

--- build.py
+++ build.py
@@ -1,24 +1,30 @@
 import os
+import subprocess
 
 try:
     os.makedirs('build')
 except:
     pass
 
-script = open('pritunl-edgemax.sh').read()
-pritunl = open('pritunl.js').read()
-edge = open('edge.php').read()
-footer = open('footer.php').read()
+script = open('wizard-run').read()
+pritunl = open('pritunl.py').read()
 
 pritunl = pritunl.replace('\\', '\\\\')
 pritunl = pritunl.replace('$', '\$')
-edge = edge.replace('\\', '\\\\')
-edge = edge.replace('$', '\$')
-footer = footer.replace('\\', '\\\\')
-footer = footer.replace('$', '\$')
 
-script = script.replace('#<pritunl>', pritunl.strip('\n'))
-script = script.replace('#<edge>', edge.strip('\n'))
-script = script.replace('#<footer>', footer.strip('\n'))
+script = script.replace('#<script>', pritunl.strip('\n'))
 
-open('build/pritunl-edgemax.sh', 'w').write(script)
+open('build/wizard-run', 'w').write(script)
+subprocess.check_call(['cp', 'validator.json', 'build/validator.json'])
+subprocess.check_call(['cp', 'wizard.html', 'build/wizard.html'])
+
+os.chdir('build')
+
+subprocess.check_call(['tar', 'cfz',
+    '../pritunl-edgemax.tar.gz',
+    'validator.json',
+    'wizard.html',
+    'wizard-run',
+])
+
+#subprocess.check_call(['rm', '-r', '../build'])
