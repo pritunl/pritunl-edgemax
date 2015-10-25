0e4a6ed475910dcdb0bcaae166d4c0bd15fd2fb2
Zachary Huff <zach.huff.386@gmail.com>
2015-10-25 17:40:24 -0400

    Improve build

--- build.py
+++ build.py
@@ -1,20 +1,26 @@
 import os
 import subprocess
 
+SCRIPT = """#!/bin/bash
+set -e
+
+cat > /tmp/pritunl.py <<- EOM
+%s
+EOM
+
+python2 /tmp/pritunl.py "$0" "$@"
+"""
+
 try:
     os.makedirs('build')
 except:
     pass
 
-script = open('wizard-run').read()
 pritunl = open('pritunl.py').read()
-
 pritunl = pritunl.replace('\\', '\\\\')
 pritunl = pritunl.replace('$', '\$')
 
-script = script.replace('#<script>', pritunl.strip('\n'))
-
-open('build/wizard-run', 'w').write(script)
+open('build/wizard-run', 'w').write(SCRIPT % pritunl.strip('\n'))
 subprocess.check_call(['cp', 'validator.json', 'build/validator.json'])
 subprocess.check_call(['cp', 'wizard.html', 'build/wizard.html'])
 
